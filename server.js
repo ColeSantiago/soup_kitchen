4f5d16432c268a21fdb5671280bdc485bd931589
Cole Santiago <colesantiago@Coles-MacBook-Pro.local>
2018-09-19 23:00:02 -0400

    heroku upload

--- server.js
+++ server.js
@@ -25,8 +25,7 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(morgan('dev'));
 app.use(cookieParser());
 
-// app.use(express.static('./client/build'));
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static('client/build'));
 
 // sets up the session for the user
 app.use(session({
