1fc243e6fe806a120d07b747e646cba225e7a207
Cole Santiago <colesantiago@Coles-MacBook-Pro.local>
2018-09-19 20:22:39 -0400

    heroku upload

--- models/index.js
+++ models/index.js
@@ -9,11 +9,11 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
   sequelize = new Sequelize(process.env.DATABASE_DATABASE, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, config);
-}
+// }
 
 fs
   .readdirSync(__dirname)
