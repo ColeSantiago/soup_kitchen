79ef14bb8107953def93d3361f06c937b81b2a99
Cole Santiago <colesantiago@Coles-MacBook-Pro.local>
2018-09-20 12:27:33 -0400

    heroku upload

--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -54,43 +54,43 @@ class Homepage extends Component {
 			      			</ScrollAnimation>
 			      		</header>
 		      		</ScrollAnimation>
-			      		<div className='info'>
-			      			<p className='history'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eleifend. 
-			      			Aliquam erat volutpat. Nunc quis risus scelerisque, dictum justo id, cursus sapien. Aliquam 
-			      			facilisis erat quis lacinia pharetra. Vestibulum aliquet justo quis enim vehicula fermentum. 
-			      			Praesent pulvinar aliquam magna nec varius. Suspendisse facilisis pretium feugiat. Cras vel 
-			      			feugiat neque, nec mollis elit. Integer at lorem in purus condimentum interdum in nec neque. 
-			      			Praesent et porta risus, efficitur feugiat nibh. Aliquam non ligula dignissim, euismod odio sed, 
-			      			vulputate magna.</p> <br></br>
-			      			<ScrollAnimation animateIn='bounceInRight' animateOnce={true}>
-			      				<p className='time'> We serve a meal every Saturday from 4pm to 5pm. </p>
-			      			</ScrollAnimation>
-			      		</div>
-			      		<div className='location'>
-			      			<p className='address'>
-				      			All Saints Catholic Academy Cafeteria <br></br>
-				      			19 West 13th Street
-				      		</p>
-			      			<div className='location-info'>
-				      			<img className='location-photo' src={LocationPhoto} alt='location' />
-				      			<Map
-				      				// eslint-disable-next-line
-								  	style='mapbox://styles/mapbox/streets-v9'
-								  	containerStyle={{
-								    	height: '400px',
-								    	width: '400px'
-								  	}}
-								  	center={[-74.125867, 40.65778]}
-								  	zoom={[14]}
-								>
-									<Marker
-										coordinates={[-74.125867, 40.65778]}
-										anchor='bottom'>
-										<img style={markerStyle} src={MapMarker} alt='marker'/>
-									</Marker>
-								</Map>
-							</div>
-			      		</div>
+		      		<div className='info'>
+		      			<p className='history'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eleifend. 
+		      			Aliquam erat volutpat. Nunc quis risus scelerisque, dictum justo id, cursus sapien. Aliquam 
+		      			facilisis erat quis lacinia pharetra. Vestibulum aliquet justo quis enim vehicula fermentum. 
+		      			Praesent pulvinar aliquam magna nec varius. Suspendisse facilisis pretium feugiat. Cras vel 
+		      			feugiat neque, nec mollis elit. Integer at lorem in purus condimentum interdum in nec neque. 
+		      			Praesent et porta risus, efficitur feugiat nibh. Aliquam non ligula dignissim, euismod odio sed, 
+		      			vulputate magna.</p> <br></br>
+		      			<ScrollAnimation animateIn='bounceInRight' animateOnce={true}>
+		      				<p className='time'> We serve a meal every Saturday from 4pm to 5pm. </p>
+		      			</ScrollAnimation>
+		      		</div>
+		      		<div className='location'>
+		      			<p className='address'>
+			      			All Saints Catholic Academy Cafeteria <br></br>
+			      			19 West 13th Street
+			      		</p>
+		      			<div className='location-info'>
+			      			<img className='location-photo' src={LocationPhoto} alt='location' />
+			      			<Map
+			      				// eslint-disable-next-line
+							  	style='mapbox://styles/mapbox/streets-v9'
+							  	containerStyle={{
+							    	height: '400px',
+							    	width: '400px'
+							  	}}
+							  	center={[-74.125867, 40.65778]}
+							  	zoom={[14]}
+							>
+								<Marker
+									coordinates={[-74.125867, 40.65778]}
+									anchor='bottom'>
+									<img style={markerStyle} src={MapMarker} alt='marker'/>
+								</Marker>
+							</Map>
+						</div>
+		      		</div>
 		    	</div>
 	    	</MuiThemeProvider>
     	);
--- server.js
+++ server.js
@@ -49,9 +49,9 @@ const routes = require('./routes');
 app.use(routes);
 
 // 404
-app.use(function (req, res, next) {
-  res.status(404).send('Sorry cant find that!')
-});
+// app.use(function (req, res, next) {
+//   res.status(404).send('Sorry cant find that!')
+// });
 
 // starting the server
 models.sequelize.sync().then(function () {
