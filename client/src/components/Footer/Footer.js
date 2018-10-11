5aca501af93bd334f9604ebe073bd949e156b1e5
Cole Santiago <colesantiago@Coles-MacBook-Pro.local>
2018-10-11 15:42:36 -0400

    fixed responsive layout for homepage

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 
 const Footer = props => 
 	<footer>
-		<p className='administration'>Administration: Blessed Miriam Teresa Parish 
+		<p className='administration'>Administration: Blessed Miriam Teresa Parish  
 			<a rel='noopener noreferrer' target='_blank' href='https://www.facebook.com/groups/126477214107724/about/'>
 				<img className='facebook-icon' src={FacebookPhoto} alt='facebook' />
 			</a>
